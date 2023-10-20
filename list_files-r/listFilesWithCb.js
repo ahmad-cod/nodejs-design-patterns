63d2d5df3dee7c84876039fd5fece04d6c9c2ad6
ahmad-cod <ahmadadewale800@gmail.com>
2023-10-20 07:13:52 +0100

    Separate the implementation with callback

---
+++ list_files-r/listFilesWithCb.js
@@ -0,0 +1,50 @@
+import fs from 'fs'
+import path from 'path'
+
+function listNestedFiles(directoryPath, callback) {
+    fs.readdir(directoryPath, (error, files) => {
+        if(error) {
+            callback(`Error reading directory path: ${error}`)
+        } else {
+            console.log(`Reaing files in ${directoryPath}:`)
+
+            function processFile (index) {
+                if (index < files.length) {
+                    const file = files[index]
+                    const filePath = path.join(directoryPath, file)
+
+                    fs.stat(filePath, (statError, fileStat) => {
+                        if (statError) {
+                            callback(`Error accessing ${filePath}: ${statError}`)
+                        }else {
+                            if(fileStat.isDirectory()) {
+                                listNestedFiles(filePath, err => {
+                                    if(err) {
+                                        callback(err)
+                                    } else {
+                                        processFile(index + 1)
+                                    }
+                                })
+                            } else {
+                                console.log(file)
+                                processFile(index + 1)
+                            }
+                        }
+                    })
+                }
+            }
+            processFile(0)
+        }
+        callback(null, files)
+    })
+}
+
+
+listNestedFiles('../web_spider', (err, files) => {
+    if (err) {
+        console.error(`Error occured: ${err.message}`)
+    }
+    else {
+        console.log('files listed successfully')
+    }
+})
