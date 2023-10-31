39de2ad9858509da7acbdf99140286cc499c811d
ahmad-cod <ahmadadewale800@gmail.com>
2023-10-31 12:33:47 +0100

    implemet readable with flowing mode

--- 06-streams/read-stdin.js
+++ 06-streams/read-stdin.js
@@ -1,11 +1,17 @@
-
 process.stdin
     .on('readable', () => {
-        console.log('New Data available')
+        console.log('New Data available in non-flowing mode')
         let chunk
 
         while ((chunk = process.stdin.read()) !== null) {
             console.log(`Chunk read ${chunk.length} bytes : ${chunk.toString()}`)
         }
+    })
+    .on('end', () => console.log('End of stream'))
+
+process.stdin
+    .on('data', (chunk) => {
+        console.log('New Data available in flowing mode')
+        console.log(`Chunk read ${chunk.length} bytes: ${chunk.toString()}`)
     })
     .on('end', () => console.log('End of stream'))
