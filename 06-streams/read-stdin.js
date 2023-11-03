7a32aa0c148736ba4e44a5e7a5d60c90c894c2e5
ahmad-cod <ahmadadewale800@gmail.com>
2023-11-03 16:00:35 +0100

    add readstream with async iterator

--- 06-streams/read-stdin.js
+++ 06-streams/read-stdin.js
@@ -1,3 +1,4 @@
+// non-flowing / paused
 process.stdin
     .on('readable', () => {
         console.log('New Data available in non-flowing mode')
@@ -9,9 +10,22 @@ process.stdin
     })
     .on('end', () => console.log('End of stream'))
 
+
+// flowing
 process.stdin
     .on('data', (chunk) => {
         console.log('New Data available in flowing mode')
         console.log(`Chunk read ${chunk.length} bytes: ${chunk.toString()}`)
     })
     .on('end', () => console.log('End of stream'))
+
+async function main() {
+    for await (const chunk of process.stdin) {
+        console.log('New data available')
+        console.log(
+            `chunk read ${chunk.length} bytes: ${chunk.toString()}`
+        )
+    }
+    console.log('End of stream')
+}
+main()
