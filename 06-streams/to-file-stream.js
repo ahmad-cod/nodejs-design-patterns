91f2ad4150636ec6f2712c2b3993e51b12f884f4
ahmad-cod <ahmadadewale800@gmail.com>
2023-11-05 16:54:11 +0100

    Implementation of custom writable

---
+++ 06-streams/to-file-stream.js
@@ -0,0 +1,17 @@
+import { promises as fs } from 'fs'
+import { Writable } from 'stream'
+import { dirname } from 'path'
+import mkdirp from 'mkdirp-promise'
+
+export class ToFileStream extends Writable {
+    constructor(options) {
+        super({ ...options, objectMode: true })
+    }
+
+    _write (chunk, encoding, cb) {
+        mkdirp(dirname(chunk.path))
+        .then(() => fs.writeFile(chunk.path, chunk.content))
+        .then(() => cb())
+        .catch(cb)
+    }
+}
